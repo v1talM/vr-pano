ea7f4820c8fcd03a7e465d71c09c98cbef65d535
v1talM <373357042@qq.com>
2017-04-15 19:50:07 +0800

    vr页面鼠标引导

---
+++ src/components/vr/mouse.vue
@@ -0,0 +1,95 @@
+<template lang="html">
+  <div class="mouse_scroll">
+		<div class="mouse">
+			<div class="wheel"></div>
+		</div>
+    <div class="notice">
+      <p>左右拖动鼠标调整方向，滚动鼠标滑轮调整距离。</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style lang="css">
+  .mouse_scroll {
+    display: block;
+    width: 10rem;
+    height: 4rem;
+    position: absolute;
+    top: calc(50% - 2rem);
+    text-align: center;
+  }
+  .mouse {
+    height: 100%;
+    width: 2.5rem;
+    border-radius: .875rem;
+    transform: none;
+    border: 2px solid white;
+    animation: mouse-move 3s linear 3;
+    margin: 0 auto;
+  }
+  .wheel {
+    display: block;
+    margin: 5px auto;
+    background: white;
+    position: relative;
+    height: .25rem;
+    width: .25rem;
+    border: 2px solid #fff;
+    border-radius: 8px;
+  }
+  .wheel {
+    -webkit-animation: mouse-wheel 0.6s linear infinite;
+    -moz-animation: mouse-wheel 0.6s linear infinite;
+  }
+  .mouse_scroll .notice p{
+    color: #ffffff;
+    font-size: .875rem;
+    margin: 1rem 0;
+  }
+  @-webkit-keyframes mouse-move{
+    0% {
+      transform: rotate(0deg);
+    }
+    25% {
+      transform: rotate(30deg);
+    }
+    50% {
+      transform: rotate(0deg);
+    }
+    75% {
+      transform: rotate(-30deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+
+  @-webkit-keyframes mouse-wheel
+  {
+   0% {
+      opacity: 1;
+      -webkit-transform: translateY(0);
+      -ms-transform: translateY(0);
+      transform: translateY(0);
+   }
+   100% {
+      opacity: 0;
+      -webkit-transform: translateY(6px);
+      -ms-transform: translateY(6px);
+      transform: translateY(6px);
+    }
+  }
+
+  @-moz-keyframes mouse-wheel
+  {
+    0% { top: 1px; }
+    25% { top: 2px; }
+    50% { top: 3px;}
+    75% { top: 2px;}
+    100% { top: 1px;}
+  }
+</style>
--- src/pages/vr/vr.vue
+++ src/pages/vr/vr.vue
@@ -1,8 +1,9 @@
 <template lang="html">
-  <div style="height: inherit">
+  <div style="height: inherit;display:flex;justify-content:center;">
     <navbar></navbar>
     <vr></vr>
     <loader v-if="preload"></loader>
+    <mouse v-if="mouseGuide"></mouse>
   </div>
 
 </template>
@@ -11,14 +12,30 @@
 import navbar from '@/components/header/op_navbar'
 import vr from '@/components/vr/vr'
 import loader from '@/components/vr/loader'
+import mouse from '@/components/vr/mouse'
 import {mapState} from 'vuex'
 export default {
   computed: mapState({
-    preload: state => state.vr.preload,
-
+    preload: state => state.vr.preload
   }),
+  data () {
+    return {
+      mouseGuide: false
+    }
+  },
+  watch: {
+    preload (status) {
+      if(!status){
+        this.mouseGuide = true
+        const vm = this
+        setTimeout(function () {
+          vm.mouseGuide = false
+        },9000)
+      }
+    }
+  },
   components: {
-    navbar, vr, loader
+    navbar, vr, loader, mouse
   }
 }
 </script>
