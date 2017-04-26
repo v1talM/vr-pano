9e816704d7ad745da54a27a255f82db08c9f091d
v1talM <373357042@qq.com>
2017-04-26 22:47:54 +0800

    crossOrigin test

--- src/components/vr/vr.vue
+++ src/components/vr/vr.vue
@@ -57,11 +57,17 @@ export default {
       this.geometry = new THREE.SphereGeometry( 500, 60, 40 );
       this.geometry.scale( - 1, 1, 1 );
       var vr_photo = url_root + this.vr.pro_photo
+      var img = new Image;
+      img.crossOrigin = 'Anonymous'
+      img.onload = function () {
+
+      }
+      img.src = vr_photo
       //this.vr_photo.src = require('@/assets/img/photo_88764.jpg')
       this.bgm_url = url_root + this.vr.pro_bgm
       var vm = this
       this.material = new THREE.MeshBasicMaterial( {
-          map: new THREE.TextureLoader().load( vr_photo, function () {
+          map: new THREE.TextureLoader().load( img.src, function () {
             vm.$store.dispatch('clearPreload')
           })
         })
